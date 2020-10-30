167f1ce20b49211f4fb8410db83be2050fd7b984
geekhch <geekhch@qq.com>
2020-10-30 17:21:51 +0800

    swa

--- code/run_train.sh
+++ code/run_train.sh
@@ -1,4 +1,4 @@
-# export ENCODER=voidful/albert_chinese_small
+
 
 # python src/train.py \
 # --model_name_or_path $ENCODER \
@@ -17,10 +17,12 @@
 # export ENCODER=hfl/chinese-bert-wwm-ext
 export ENCODER=hfl/chinese-roberta-wwm-ext
 # export ENCODER=hfl/chinese-roberta-wwm-ext-large
+# export ENCODER=voidful/albert_chinese_small
+
 export OUTPUT_DIR=./output
 # # export ENCODER=allenyummy/chinese-bert-wwm-ehr-ner-sl
 
-python src/train.py --model_name_or_path $ENCODER --output_dir $OUTPUT_DIR --pretrained_cache_dir ./pretrained --batch_size 8 --lr 5e-5 --max_epoches 7 --max_steps 20000 --warmup_steps 100 --num_workers 1 --gpu_id 1
+python code/train.py --model_name_or_path $ENCODER --output_dir $OUTPUT_DIR --pretrained_cache_dir ./pretrained --batch_size 8 --lr 5e-5 --max_epoches 20 --max_steps 20000 --warmup_steps 100 --num_workers 1 --gpu_id 1
 
 # KFolds 规则
 # python src/train.py --model_name_or_path $ENCODER --output_dir $OUTPUT_DIR --pretrained_cache_dir ./pretrained --batch_size 8 --lr 5e-5 --max_epoches 5 --max_steps 20000 --warmup_steps 100 --num_workers 2 --gpu_id 1 --k_folds 0/40
