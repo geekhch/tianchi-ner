43f41ff206f9844ce8b3fba30ec1ec164646b2a8
geekhch <geekhch@qq.com>
2020-11-01 20:53:12 +0800

    docker and train params

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,16 @@
 # ENV NVIDIA_DRIVER_CAPABILITIES compute,utility
 # Base Images
 ## 指定了版
-FROM registry.cn-shenzhen.aliyuncs.com/tianchi-tcm/tianchi-sz:model
-# FROM registry.cn-shenzhen.aliyuncs.com/tianchi-tcm/tianchi-sz:base
+# FROM registry.cn-shenzhen.aliyuncs.com/tianchi-tcm/tianchi-sz:model
+FROM registry.cn-shenzhen.aliyuncs.com/tianchi-tcm/tianchi-sz:base
 
-# RUN mkdir -p /tianchi/user_data
+RUN mkdir -p /tianchi/user_data
 
 ## 把当前文件夹里的文件构建到镜像的根目录下
 
 # COPY ./code/run_predict.sh /tianchi/code/run_predict.sh
-COPY . /tianchi/
-# COPY ./user_data /tianchi/user_data/
+# COPY . /tianchi/
+COPY ./user_data /tianchi/user_data/
 
 ## 指定默认工作目录为根目录（需要把run.sh和生成的结果文件都放在该文件夹下，提交后才能运行）
 WORKDIR /tianchi/
--- code/run_predict.sh
+++ code/run_predict.sh
@@ -6,26 +6,16 @@ cp ./data/juesai/* ./tmp_data/test1/
 
 export ENCODER=hfl/chinese-roberta-wwm-ext
 
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds_no_crf/step_4188
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds_no_crf/step_2796
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds_no_crf/step_4209
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds_no_crf/step_4242
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds_no_crf/step_7065
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds_no_crf/step_5508
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds_no_crf/step_2834
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds_no_crf/step_7100
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds_no_crf/step_4251
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds_no_crf/step_7085
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds/5/step_5592
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds/3/step_2792
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds/6/step_2834
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds/4/step_7070
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds/1/step_2834
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds/9/step_2806
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds/8/step_7100
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds/2/step_5508
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds/0/step_2834
-python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/kfolds/7/step_7065
+python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/swa-nocrf-words-kfolds/0/epoch_5
+python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/swa-nocrf-words-kfolds/1/epoch_5
+python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/swa-nocrf-words-kfolds/2/epoch_5
+python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/swa-nocrf-words-kfolds/3/epoch_5
+python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/swa-nocrf-words-kfolds/4/epoch_5
+python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/swa-nocrf-words-kfolds/5/epoch_5
+python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/swa-nocrf-words-kfolds/6/epoch_5
+python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/swa-nocrf-words-kfolds/7/epoch_5
+python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/swa-nocrf-words-kfolds/8/epoch_5
+python code/predict.py --model_name_or_path $ENCODER --data_dir tmp_data --pretrained_cache_dir ./user_data/pretrained --batch_size 4 --model_dir user_data/swa-nocrf-words-kfolds/9/epoch_5
 
 
 python code/volt.py
